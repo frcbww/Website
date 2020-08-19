d6c9a2a45065a31d43e5369f51425dfc1d3bff57
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-19 12:16:19 +0900

    ヘッダーの透過度変更

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // 一定以上スクロールしたらヘッダーの色を変更する
 $(window).on('scroll', function () {
-    if ($(window).scrollTop() > $(window).height() / 4) {
+    if ($(window).scrollTop() > $(window).height() / 3) {
         $('header').addClass('transform');
     } else {
         $('header').removeClass('transform');
