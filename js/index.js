0015b49f7624e79347b7ab8a46f5003b05f62616
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-20 16:16:11 +0900

    コンテンツセクションにスクロールする背景画像を追加

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-// 一定以上スクロールしたらヘッダーの色を変更する
 $(window).on('scroll', function () {
+    let scroll = $(document).scrollTop();
+
+    // 一定以上スクロールしたらヘッダーの色を変更する
     if ($(window).scrollTop() > $(window).height() / 3) {
         $('header').addClass('transform');
     } else {
         $('header').removeClass('transform');
     }
+
+    // 背景画像をスクロールする
+    $('.content-bg').stop(true, true).animate({
+        'background-position-y': scroll / 3 - 500 + 'px'
+    }, 100);
 });
