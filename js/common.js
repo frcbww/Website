9fdfc7fd5c0651a41019efbb5bb8dd594ec62b4e
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-30 20:26:13 +0900

    画像の遅延読み込み・WebP置き換え処理追加

--- js/common.js
+++ js/common.js
@@ -7,14 +7,52 @@ $(window).on('load scroll', function () {
     }
 });
 
+
 $(function () {
-    const trigger = $('#hm-menu-trigger');
+    // WebP対応ブラウザの場合パス置き換え
+    Modernizr.on('webp', function (result) {
+        $(".lazy").each(function () {
+            if (result) {
+                const split = $(this).attr('data-src').match(/(.*\/)?(.*?)\.(\w+)?/);
+                $(this).attr('data-src', split[1] + 'webp/' + split[2] + '.webp');
+            }
+        });
+    });
+
+    // 画像の遅延読み込み
+    const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+    if ("IntersectionObserver" in window) {
+        lazyImages.forEach(function (image) {
+                image.src = "data:image/gif;base64,R0lGODlhAQABAGAAACH5BAEKAP8ALAAAAAABAAEAAAgEAP8FBAA7";
+            }
+        )
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    if (typeof lazyImage.dataset.srcset === "undefined") {
+                    } else {
+                        lazyImage.srcset = lazyImage.dataset.srcset;
+                    }
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function (lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    }
+
     // ハンバーガーメニューのクリックリスナー
     $('.hamburger-menu').on('click', function () {
         hamburger_menu_toggle();
     });
 
     // ハンバーガーメニューの領域外クリック時にOFF切り替え
+    const trigger = $('#hm-menu-trigger');
     $(document).on('click', function (e) {
         if (trigger.hasClass('active') && !$(e.target).closest('header').length) {
             hamburger_menu_toggle();
