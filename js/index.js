9fadff0d06e78506553777ece9f90f078831f6b3
AokiYuki <yuukiyuuki223@gmail.com>
2020-09-01 01:55:56 +0900

    100vh再設定をjqueryからjsに置き換え

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
+// 背景画像を視差効果でスクロールする
 window.onscroll = function () {
     const scrollTop = document.scrollingElement.scrollTop;
     const content_bg = document.getElementsByClassName("content-bg")[0];
     content_bg.style.backgroundPositionY = scrollTop / 3 - 500 + "px";
 }
 
-$(window).on('load resize', function () {
-    // スマートフォン・タブレットの判別
+window.onresize = function () {
+    // スマートフォン・タブレットの場合100vh再設定
     if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0
         && navigator.userAgent.indexOf('Mobile') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-        const heightSize = $(window).height();
-        $('.top-section').height(heightSize);
-        $('.top-video').height(heightSize);
+        const heightSize = window.innerHeight;
+        document.getElementsByClassName('top-section')[0].style.height = heightSize + 'px';
+        document.getElementsByClassName('top-video')[0].style.height = heightSize + 'px';
     }
-});
+}
 
 $(function () {
     // スクロールボタン
